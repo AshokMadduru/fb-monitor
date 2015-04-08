6977d6cf08a6ca1446a71ae6f9942c5808af3db2
Ashok Madudru <ashokgirihari@gmail.com>
2015-04-08 09:59:04 +0530

    sharing option is added

--- data/content.js
+++ data/content.js
@@ -3,11 +3,36 @@
 // contents with the message payload.
 
 // get mouse-down events
+var prev = " ";
 document.onmousedown = function(data){
 	console.log(data.target.className);
-	if(data.target.className == "_42ft _4jy0 _11b _4jy3 _4jy1 selected _51sy")
+	if(data.target.className == "_42ft _4jy0 _11b _4jy3 _4jy1 selected _51sy"){
 		self.port.emit("mouse","Status posted");
-	else if(data.target.className == "_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy") self.port.emit("mouse","Shared");
-	else if(data.target.parentNode.className == "UFILikeLink") self.port.emit("mouse","Liked");
+		prev = " ";
+	}else if(data.target.className == "_4jy0 _4jy3 _4jy1 _51sy selected _42ft") {
+		self.port.emit("mouse","Shared");
+		prev = " ";
+	}else if(data.target.parentNode.className == "UFILikeLink"){
+		self.port.emit("mouse","Liked");
+		prev = " ";
+	}else if(data.target.className == "_209g _2vxa"){
+		self.port.emit("mouse", "clicked on comment");
+		prev  = "comment";
+	}else if(data.target.parentNode.className == "comment_link"){
+		prev = "comment";
+	}/* else if(data.target.className = "share_action_link"){
+		prev  = "share";
+	} */
+}
+
+document.onkeydown = function(data){
+	console.log(data.key);
+	if(data.key == "Enter"){
+		console.log("clicked on enter");
+		if(prev == "comment"){
+			console.log("commented");
+		}
+		prev  = " ";
+	}
 }
 
